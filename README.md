d8c7e67c337b5ce7105e11af44de998a37060570
Salvatore Laisa <moebiusmania@gmail.com>
2016-01-02 23:44:05 +0100

    dati da DB esterno (Firebase)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# sganauei-bot
+# Sganauei-bot
+### Il bot di Telegram che ti aiuta il sabato sera :)
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-var TelegramBot = require('node-telegram-bot-api');
+'use strict';
 
+// Imports
+var TelegramBot = require('node-telegram-bot-api');
+var fetch = require('node-fetch');
+fetch.Promise = require('bluebird');
+
+// Setup token
 var token = require('./token.js');
+
 // Setup polling way
 var bot = new TelegramBot(token, {polling: true});
-
-// Data
-var birre = ['Weiss','Kellerbier','Guinness','Birra piscio'];
 
 // Utilities
 function randomize(length){
@@ -18,28 +22,40 @@ bot.onText(/\/echo (.+)/, function (msg, match) {
   bot.sendMessage(fromId, resp);
 });
 
+// bot.onText(/\/cosabevo/, function (msg, match) {
+//   var fromId = msg.chat.id ? msg.chat.id : msg.from.id;
+//   var resp = match[1];
+//   bot.sendMessage(fromId, 'Sono ancora in fase di sviluppo, per ora puoi chiedermi consigli tra:');
+//   bot.sendMessage(fromId, '- birre');
+//   bot.sendMessage(fromId, '- analcolici (a tuo rischio e pericolo)');
+// });
+
 bot.onText(/\/cosabevo (.+)/, function (msg, match) {
-  var fromId = msg.from.id;
-  var fromIdGroup = msg.chat.id;
+  var fromId = msg.chat.id ? msg.chat.id : msg.from.id;
   var resp = match[1];
   var num = 0;
+  let myData;
   // console.log(msg);
-  // console.log(match);
+  console.log(match);
   switch(match[1]){
     case 'birra':
-      num = randomize(birre.length);
-      console.log(msg);
-      console.log(match);
-      bot.sendMessage(fromIdGroup, msg.from.first_name + ' che ne dici di una ' + birre[num] + '?');
+      bot.sendMessage(fromId,'fammi pensare...');
+      fetch('https://sganauei-bot.firebaseio.com/beers.json')
+        .then((res) => {
+          return res.json();
+        }).then((json) => {
+          myData = json;
+          num = randomize(myData.length);
+          bot.sendMessage(fromId, msg.from.first_name + ' che ne dici di una ' + myData[num] + '?');
+        });
       break;
     case 'analcolico':
-      bot.sendMessage(fromIdGroup, msg.from.first_name + ', se vuoi dell\'acqua puoi infilare la testa nell\'abbeveratoio dei cavalli...');
+      bot.sendMessage(fromId, msg.from.first_name + ', se vuoi dell\'acqua puoi infilare la testa nell\'abbeveratoio dei cavalli...');
       break;
     default:
-      bot.sendMessage(fromIdGroup, msg.from.first_name + ' almeno dimmi cosa!');
+      bot.sendMessage(fromId, 'Ancora non ho consigli a riguardo 😭');
       break;
   }
-  // bot.sendMessage(fromId, resp);
 });
 
 // Any kind of message
